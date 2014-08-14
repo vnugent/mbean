6032b2408a993bd7edb2a1d1831e5a89105d3d25
Viet Nguyen <vnguyen@redhat.com>
2014-08-14 12:18:24 -0700

    start script

---
+++ start.sh
@@ -0,0 +1,2 @@
+MBEAN_COUNT=50
+java  -Dcom.sun.management.jmxremote.port=9999 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false -jar mbean.jar ${MBEAN_COUNT}
